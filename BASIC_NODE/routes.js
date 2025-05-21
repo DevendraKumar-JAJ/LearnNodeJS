a3fa001f25a7c87a5b8854cdb5dd4b041005ee05
Devendra Kumar <dkumar06545@gmail.com>
2025-05-21 20:03:32 +0530

    Basic routes concept.js

---
+++ BASIC_NODE/routes.js
@@ -0,0 +1,34 @@
+const http=require('http');
+
+const server=http.createServer((req,res)=>{
+  if(req.url==='/'){
+    res.end('Home');
+
+  }
+  else if (req.url==='/about'){
+    // res.end('<h1>About</h1>')
+    res.setHeader("Content-Type", "text/html");
+   res.end('<marquee behavior="scroll" direction="left"><h1></h1></marquee>');
+  }
+  else if(req.url=='/contact'){
+    res.setHeader("Content-Type", "text/html");
+    res.end('<h1>Cotact </h1><h2>Agin</h2>')
+  }
+})
+
+server.listen(4000,()=>{
+  console.log(4000)
+});
+
+// let array=process.argv
+// array.forEach(ele => {
+//   console.log(`Hello ${ele}`)
+// });
+
+
+// mmon values:
+
+// 1. scroll: The content scrolls continuously in the specified direction.
+// 2. slide: The content slides into view and stops when it reaches the edge.
+// 3. alternate: The content scrolls back and forth between the edges.
+
